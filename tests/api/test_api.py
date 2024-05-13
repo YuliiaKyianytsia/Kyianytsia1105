12857c2da947b9fd5e42b4d786f95f1a2a61d62a
YuliiaKyianytsia <73847153+YuliiaKyianytsia@users.noreply.github.com>
2024-05-13 11:11:22 +0300

    Update test_api.py

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,19 +3,19 @@ import pytest
 class User:
 
     def __init__(self) -> None:
-        self.name = "Sergii"
-        self.second_name = "Butenko"
+        self.name = 'Sergii'
+        self.second_name = 'Butenko'
 
 @pytest.fixture
 def user():
     yield User()
 
 def test_remove(user):
-    user.name == ""
-    assert user.name == ""
+    user.name == ''
+    assert user.name == ''
 
 def test_name(user):
-    assert user.name == "Sergii"
+    assert user.name == 'Sergii'
 
 def test_second_name(user):
-    assert user.second_name == "Butenko"
+    assert user.second_name == 'Butenko'
