f99b071f5cc7a2860de1f6e5f44c4b51db3b505b
YuliiaKyianytsia <73847153+YuliiaKyianytsia@users.noreply.github.com>
2024-05-16 13:35:50 +0300

    Update test_http.py

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -6,3 +6,7 @@ def test_first_request():
     r = requests.get('http://api.github.com/zen')
     print(r.text)
   
+@pytest.mark.http
+def test_second_request():
+    r = requests.get('http://api.github.com/users/defunct')
+    print(f"Response is {r.text}")
