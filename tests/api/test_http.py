1dc0705f8e5177ccdda1ca18c52c4782ef8fb158
YuliiaKyianytsia <73847153+YuliiaKyianytsia@users.noreply.github.com>
2024-05-21 12:09:15 +0300

    Update test_http.py

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -9,7 +9,7 @@ def test_first_request():
   
 @pytest.mark.http
 def test_second_request():
-    
+    r = requests.get('http://api.github.com/users/defunct')   
     print(f"Response Body is {r.json()}")
     print(f"Response Status code is {r.status_code}")
     print(f"Response Headers are {r.headers}")
